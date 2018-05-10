e6ac8a4f49ebd0684b947c2565916a1c491049ce
mopemope <yutaka.matsubara@gmail.com>
2018-05-11 08:47:04 +0900

    Improve import-at-point

--- meghanada.el
+++ meghanada.el
@@ -913,6 +913,31 @@ e.g. java.lang.annotation)."
       (save-buffer)
       (meghanada-optimize-import))))
 
+(defun meghanada-import-at--callback (result buf optimize)
+  "TODO: FIX DOC RESULT BUF OPTIMIZE."
+  (with-current-buffer buf
+    (when result
+      (mapc
+       (lambda (imports)
+         (let ((type (car imports))
+               (imps (cdr imports)))
+           (pcase type
+             (`class
+              (if (= (length imps) 1)
+                  (meghanada--add-import (car imps) buf)
+                (let ((res (completing-read "import:" imps nil t)))
+                  (unless (string= res "")
+                    (meghanada--add-import res buf)))))
+             (`method
+              (if (= (length imps) 1)
+                  (meghanada--add-import (car imps) buf)
+                (let ((res (completing-read "import:" imps nil t)))
+                  (unless (string= res "")
+                    (meghanada--add-import res buf)))))))) result))
+    (when optimize
+      (save-buffer)
+      (meghanada-optimize-import))))
+
 ;;
 ;; meghanada client api
 ;;
@@ -1031,7 +1056,7 @@ e.g. java.lang.annotation)."
 (defun meghanada-import-at-point ()
   "TODO: FIX DOC ."
   (interactive)
-  (meghanada-import-at-point-async #'meghanada-import-all--callback (current-buffer) nil))
+  (meghanada-import-at-point-async #'meghanada-import-at--callback (current-buffer) nil))
 
 
 ;;
@@ -1133,8 +1158,8 @@ e.g. java.lang.annotation)."
     (kill-buffer name)))
 
 (defun meghanada--compile-callback (result)
-  "TODO: FIX DOC OUTPUT ."
-  (let* ((severity (car result)))
+  "TODO: FIX DOC RESULT ."
+  (let ((severity (car result)))
     (pcase severity
       (`success
        (progn
